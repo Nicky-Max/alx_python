54f69e03f44a088b74c13968e05e2b6dc30caae7
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 15:39:29 +0300

    task 4

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 def raise_exception(x=""):
-    x = "Exception raised"    
+    
     try:
         raise TypeError(x)
     except TypeError as te:
         print(te)
 
 if __name__ == "__main__":
-    raise_exception(x="Exeption raised")       
+    raise_exception("Exeption raised")       
    
 
 
