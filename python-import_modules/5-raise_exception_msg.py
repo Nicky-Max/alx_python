55f8acef453b830461bde28cd42fb160da7e6a04
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 14:59:10 +0300

    task 5 updated

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-
 def raise_exception_msg(message=""):
-    message ="C is fun"
- 
     try:
         raise NameError(message)
     except NameError as ne:
         print(ne)
-        
+
+if __name__ == "__main__":
+    raise_exception_msg("C is fun")
+
 
    
