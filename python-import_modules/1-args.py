c3d325048275f9515f2fd190f22f75490f9acc4c
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 12:47:05 +0300

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
+def print_arguments(argv):
+    num_arguments = len(argv)
+
+    if num_arguments == 0:
+        print("0 argument.")
+    elif num_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arguments))
+
+    if num_arguments == 0:
+        print(".")
+    else:
+        for i, arg in enumerate(argv):
+            print("{} : {}".format(i+1, arg))
+
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-    args_list = sys.argv[1:]  
-
-    print("Number of argument(s): {}:".format(num_args), end=' ')
-    print("argument" if num_args == 1 else "arguments", end='')
-    print(":" if num_args > 0 else ".", end='\n')
-
-    for i, arg in enumerate(args_list, start=1):
-        print("{}: {}".format(i, arg))
+    print_arguments(sys.argv[1:])
