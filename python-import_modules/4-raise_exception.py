b640e342298aad9877087e73d6648519c7ef2c4d
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 13:24:44 +0300

    task 4

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def raise_exception():
+    try:
+        raise TypeError("This is a custom type exception.")
+    except TypeError as e:
+        print("Exception raised:", e)
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
