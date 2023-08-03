e000e1cd5e7c839b51ea1b064b6c57c0e5e0f577
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 13:11:27 +0300

    task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -11,7 +11,7 @@ def safe_print_division(a, b):
 
     safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-a = 12
-b = 0
+a = 4
+b = -2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
