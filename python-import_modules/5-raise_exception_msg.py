a588bf0c44bdf34dd0ed144f1b3e2378be5da870
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 14:41:51 +0300

    task 5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
+
 def raise_exception_msg(message=""):
-    message = "C is fun" 
+ 
     try:
         raise NameError(message)
     except NameError as ne:
         print(ne)
 
-     
+raise_exception_msg()    
