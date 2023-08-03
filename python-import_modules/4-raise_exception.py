eedfad0a6e0ea690d6fff15f060b7e2982fd602d
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-03 13:27:27 +0300

    task 4

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,9 +2,9 @@
 
 def raise_exception():
     try:
-        raise TypeError("This is a custom type exception.")
-    except TypeError as e:
-        print("Exception raised:", e)
+        raise TypeError()
+    except TypeError as te:
+        print("Exception raised:", te)
    
 
 
