c1ddaba3fc2ba5b7245dd5b6c50b8bcbc6cfeaa8
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-09 22:34:49 +0300

    task 0 changed

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 class Square:
-    def __init__(self, size):
-        """
+     
+     """
         This functions allows the user to specify the size of the square at every instance
 
         Arg: 
            size here is a private attribute since the  size of a square is crucial for a square, many things depend of it
-        """
+    """
+     def __init__(self, size):
+        
         self.__size = size
 
