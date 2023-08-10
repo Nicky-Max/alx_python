59cebc51190efb05de7f200667fc3d319765c38a
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-10 11:33:00 +0300

    task 1 changes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
-Square = __import__('0-square').Square
+class Square:
+    """
+    This class defines a square by a private instance attribute 'size'.
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+    Attributes:
+        __size (int): The size of the square.
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance with the given size.
+    """
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with the given size.
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
