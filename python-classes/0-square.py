0b2854f8460488d5564747124ada1acc12d28afb
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-10 11:53:39 +0300

    changes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -21,17 +21,5 @@ class Square:
 print(__import__("0-square").Square.__doc__)
 print(__import__("0-square").Square.__init__.__doc__)  
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
 
