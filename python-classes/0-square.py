f9953ab7c045ef6e7006b097983c5e5dde9c57e9
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-10 11:50:33 +0300

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -19,5 +19,19 @@ class Square:
         """
         self.__size = size
 print(__import__("0-square").Square.__doc__)
-print(__import__("0-square").Square.__init__.__doc__)      
+print(__import__("0-square").Square.__init__.__doc__)  
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
 
