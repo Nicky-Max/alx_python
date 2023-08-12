257d1cee27f22dff846c82aca3c432d42758f861
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-12 23:10:14 +0300

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,14 @@
+"""
+Fetches a URL using the requests package and displays the body response.
+"""
 import requests
 
 def fetch_hbtn_status():
     url = 'https://alu-intranet.hbtn.io/status'
+    # Send a GET request to the URL
     response = requests.get(url)
-
+    
+    # Display the body response details
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
