e6acb1c9146e84b2f8735acb550598f4c31f3e67
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-09-23 20:28:14 +0300

    tasks1,2

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that responds to the root URL '/'
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0 and port 5000
+    app.run(host='0.0.0.0', port=5000)
