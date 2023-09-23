785cf77f1ea98f9b85e1645bd897a24a775e5145
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-09-23 20:33:32 +0300

    changes

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Define a route that responds to the root URL '/'
+""" Define a route that responds to the root URL '/'
+"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the Flask app on 0.0.0.0 and port 5000
+    """
+        Run the Flask app on 0.0.0.0 and port 5000
+    """
     app.run(host='0.0.0.0', port=5000)
