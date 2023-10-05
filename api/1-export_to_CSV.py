7e7bbfbc3f27d8af4946a497fd9ffe17e2d1ef17
Nicky-Max <nicholaskipruto112@gmail.com>
2023-10-05 17:39:14 +0300

    task1 changes

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,48 +1,33 @@
 import requests
-import sys
 import csv
 
-def get_employee_data(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+def get_employee_todo_progress(employee_id):
+    # Define the API endpoints
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_endpoint = f"{base_url}/users/{employee_id}"
+    todo_endpoint = f"{base_url}/todos?userId={employee_id}"
 
-    """ Fetch employee data"""
-    employee = requests.get(base_url).json()
-    user_id = employee["id"]
-    username = employee["username"]
+    # Fetch employee details
+    employee_response = requests.get(employee_endpoint)
+    employee_data = employee_response.json()
+    user_id = employee_data["id"]
+    username = employee_data["username"]
 
-    """ Fetch TODO list for the employee"""
-    todos = requests.get(todos_url).json()
+    # Fetch TODO list
+    todo_response = requests.get(todo_endpoint)
+    todo_data = todo_response.json()
 
-    """ Export data in CSV format"""
-    csv_file_path = "{}.csv".format(user_id)
-    with open(csv_file_path, mode='w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    # Create a CSV file for the employee
+    csv_filename = f"{user_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
+        # Write the CSV header
         writer.writeheader()
 
-        for todo in todos:
+        # Write TODO list data to the CSV file
+        for task in todo_data:
             writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': username,
-                'TASK_COMPLETED_STATUS': str(todo['completed']),
-                'TASK_TITLE': todo['title']
+                "USER_ID": user_id,
             })
-
-    print(f"Data exported to {csv_file_path}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-
-    try:
-        employee_id = int(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    get_employee_data(employee_id)
